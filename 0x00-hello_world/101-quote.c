e8e5c0bac62a27274b91413827f34907a7324afa
fonnde <fonnde919@gmail.com>
2023-02-16 14:36:38 -0800

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to standard error
- * Return: 1 (success)
+ * main - prints exactly "and that piece of art is useful"
+ *
+ *   Return: Always 0 (success)
  */
 int main(void)
-{	fputs("and that piece of art is useful\" - Dora Korpar,
-	2015-10-19\n", stdout);
+{	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
