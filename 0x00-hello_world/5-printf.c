76c3fdb631518f16ce7d42862244f342f4154f2b
fonnde <fonnde919@gmail.com>
2023-02-16 08:52:06 -0800

    fesh

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
  * Return: 0 always
  */
 int main(void)
-{	printf("with proper grammar, but the outcome is a piece of art,");
+{	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
