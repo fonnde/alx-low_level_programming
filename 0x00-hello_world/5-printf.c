5e555c5dc1f49155f4e8cd6c177bddb1624226cd
fonnde <fonnde919@gmail.com>
2023-02-16 08:49:22 -0800

    fetch

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this prints with proper grammar, but the outcome is a piece of art
+ *
+ * Return: 0 always
+ */
+int main(void)
+{	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
