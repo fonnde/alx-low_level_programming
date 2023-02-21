eea639950072acdcd1b9da3f46c92721606ac511
fonnde <fonnde919@gmail.com>
2023-02-21 03:51:04 -0800

    fetch

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,9 +3,12 @@
  * prints_ alphabet-print all alphabets in lower case
  * written by brandon
  */
-void print_alphabet(void);
-{	char letter;
+void print_alphabet(void)
+{
+	char (letter);
 	for (letter = 'a'; letter <= 'z'; letter++)
-	_putchar(letter)
-	_putchar('\n');
+	{
+		_putchar(letter);
+		_putchar('\n');
+	}
 }
