7cd129b7e00c1b701c4e0b9ab4593cc425cb1313
fonnde <fonnde919@gmail.com>
2023-02-21 03:12:15 -0800

    puchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * prints_ alphabet-print all alphabets in lower case
+ * written by brandon
+ */
+void print_alphabet(void);
+{	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar(letter)
+	_putchar('\n');
+}
