4f78c733527df3c4298cba43617d70238c72c549
fonnde <fonnde919@gmail.com>
2023-04-05 02:39:54 -0700

    jj

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+  * add_nodeint_end - add node at end of a listint_t list.
+  * descrubtion: return null if not fullfiled
+  * @head: head of  pointer
+  * @n: int addition to all  list
+  * Return: NULL if condition not fullfiled
+  */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *temp;
+	if (head == NULL)
+	return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	return (NULL);
+		new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+	*head = new;
+		return (new);
+	}
+	
+	temp = *head;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = new;
+	return (new);
+}
