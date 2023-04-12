3bb5892c5ed9b415e1713e55bc72d251e167e221
fonnde <fonnde919@gmail.com>
2023-04-12 02:49:53 -0700

    jj

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,41 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * read_textfile - read a file to be attributed
  *
  * @filename: the file to be read.
  * @letters: the number of chars to read
  * Description: go through a file to know output
- *
  * section header: the header of this function is main.h
- * Return: this return the number of chars
+ * Return: If the function fails or filename is NULL - 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, _read, _write;
+	ssize_t a, b, c;
 	char *buffer;
 
-	if (filename == NULL)
-		return (0);
 
+
+	if (filename == NULL)
+	return (0);
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	return (0);
 
-	fd = open(filename, O_RDONLY, 600);
-	if (fd == -1)
 
-	{
-		free(buffer);
-	return (0);
-	}
-	_read = read(fd, buffer, letters);
-	if (_read == -1)
+	a = open(filename, O_RDONLY);
+	b = read(a, buffer, letters);
+	c = write(STDOUT_FILENO, buffer, b);
+
+	if (a == -1 || b == -1 || c == -1 || c != b)
 	{
 	free(buffer);
 	return (0);
 	}
 
-	_write = write(STDOUT_FILENO, buffer, _read);
-	if (_write == -1 || _write != _read)
-	{
-	return (0);
-	}
-
 	free(buffer);
-	close(fd);
-	return (_write);
+	close(a);
+
+	return (c);
 }
